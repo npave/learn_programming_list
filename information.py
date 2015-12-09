8418b016e1e0aa3eb2562e484db35b5dfd7d745b
Nancy Pacheco <nancydanielap@gmail.com>
2015-12-08 18:56:32 -0800

    First version.

---
+++ information.py
@@ -0,0 +1,7 @@
+class Resource():
+    def __init__(self, name, description, style, language):
+        self.name        = name
+        self.description = description
+        self.style       = style
+        self.language    = language
+
